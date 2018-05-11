00f834edd1ebf7bfe64e8b5f26a913022cf8b828
Saicharan <vsaicharan1998@gmail.com>
2018-05-11 15:37:18 +0530

    Data processing almost done

--- lstm.py
+++ lstm.py
@@ -3,33 +3,35 @@ import tensorflow as tf
 
 import csv
 
+INPUT_SIZE = 3
+NUM_STEPS = 2
 
 def read_csv_file(filename):
     name = filename
 
-    # initializing the titles and rows list
+    """initializing the titles and rows list"""
     fields = []
     rows = []
 
-    # reading csv file
+    """reading csv file"""
     with open(name, 'r') as csvfile:
-        # creating a csv reader object
+        """creating a csv reader object"""
         csvreader = csv.reader(csvfile)
 
-        # extracting field names through first row
+        """extracting field names through first row"""
         fields = csvreader.next()
 
-        # extracting each data row one by one
+        """extracting each data row one by one"""
         for row in csvreader:
             rows.append(row)
 
-        # get total number of rows
+        """get total number of rows"""
         print("Total no. of rows: %d"%(csvreader.line_num))
 
-    # printing the field names
+    """printing the field names"""
     print('Field names are:' + ', '.join(field for field in fields))
 
-    #  printing first 5 rows
+    """printing first 5 rows"""
     print('\nFirst 5 rows are:\n')
     for row in rows[:5]:
 
@@ -41,5 +43,61 @@ def read_csv_file(filename):
     return rows
 
 
-data = read_csv_file('USD_INR.csv')
-print(data)            
+def read_prices():
+    data = read_csv_file('USD_INR.csv')
+    prices = []
+
+    """reversing data (to predict future prices)"""
+    for row in (data[::-1]):
+
+        prices.append(row[1])
+
+    return prices
+
+
+def split_data(input):
+
+    """
+    Splits a sequence into windows
+    """
+
+    seq = [np.array(input[i * INPUT_SIZE: (i + 1) * INPUT_SIZE])
+       for i in range(len(input) // INPUT_SIZE)]
+
+    """Split into groups of `num_steps"""
+    X = np.array([seq[i: i + NUM_STEPS] for i in range(len(seq) - NUM_STEPS)])
+    y = np.array([seq[i + NUM_STEPS] for i in range(len(seq) - NUM_STEPS)])
+
+
+    return X , y
+
+
+def train_test_split(X , y):
+
+    """
+    Splitting data into training and test data"
+    """
+
+    X_train = X[0:3000]
+    X_test= X[3000:]
+
+    y_train = y[0:3000]
+    y_test = y[3000:]
+
+    """
+    Validating shapes
+
+    print("X_train" , X_train.shape)
+    print("Y_train" , y_train.shape)
+    print("X_test" , X_test.shape)
+    print("Y_test" , y_test.shape)
+
+    """
+
+    return X_train , y_train , X_test , y_test
+
+
+prices = read_prices()
+X , y = split_data(prices)
+
+X_train , y_train , X_test , y_test = train_test_split(X,y)
